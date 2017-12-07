8535ba2111f218f13a223b6eb5ffd7703b358789
Nic Patterson <arasureynn@gmail.com>
2017-12-06 20:11:14 -0800

    Constants file?

---
+++ src/com/concur/Constants.groovy
@@ -0,0 +1,6 @@
+public class Constants {
+  public static String clearColor = "\u001B[0m"
+  public static String debugColor = "\u001B[35m"
+  public static String debugMsgColor = "\u001B[36m"
+  public static String debugTitleColor = "\u001B[34m"
+}
