994f0ed9f87c21f1de7d2a65cebdda9aaae4675e
Nic Patterson <arasureynn@gmail.com>
2018-03-17 18:46:55 -0700

    Start of new versioning

---
+++ src/com/concur/Versioning.groovy
@@ -0,0 +1,60 @@
+#!/usr/bin/env groovy
+package com.concur
+
+import groovy.transform.Field
+
+@Field def concurPipeline = new Commands()
+@Field def concurUtil     = new Util()
+@Field def concurGit      = new Git()
+
+/*
+description: |
+  Determine a version number based on the current latest tag in the repository. Will automatically increment the minor version and append a build version.
+  ```yaml
+  pipelines:
+    general:
+      version:
+        base: 1.0.0
+        scheme: semantic
+        versionImage: quay.io/reynn/docker-versioner:0.1.0
+        pattern: "^.*.*"
+  ```
+examples:
+  - |
+    // Latest tag in the repo is 1.3.1 and it was tagged 5 hours ago
+    println new com.concur.Versioning().getVersion(yml)
+    // 1.4.0-0018000000
+  - |
+    // New repo with no tags, repository was created 1 hour ago
+    println new com.concur.Versioning().getVersion(yml)
+    // 0.1.0-0003600000
+  - |
+    // No tags in repo, override default version, created 18 days ago
+    println new com.concur.Versioning().getVersion(yml)
+    // 3.7.0-1555200000
+ */
+def getVersion(Map yml) {
+  if (env."${Constants.Env.VERSION}") {
+    concurPipeline.debugPrint('Returning previously determined version.', 3)
+    return env."${Constants.Env.VERSION}"
+  }
+  String branchPattern  = concurPipeline.checkBranch(yml)
+  String dockerImage    = yml.general?.version?.versionImage  ?: 'quay.io/reynn/docker-versioner:0.2.0'
+  String executable     = yml.general?.version?.executable    ?: 'versioning'
+
+  List envs = concurUtil.mustacheReplaceAll((yml.general?.version ?: [:]).collect{ 
+    "versioning_${it.key}=${concurUtil.mustacheReplaceAll(it.value)}"
+  }.join(';')).split(';')
+
+  concurPipeline.debugPrint(yml.general?.version)
+  
+  String returnVal = ''
+
+  withEnv(envs) {
+    docker.image(dockerImage).inside {
+      returnVal = (sh returnStdout: true, script: executable).trim()
+    }
+  }
+  env."${Constants.Env.VERSION}" = returnVal
+  return output
+}
